00ae62413071497ef7666374fe59f4c5189f9773
Hadam Cho <chohadam49@gmail.com>
2023-02-13 18:09:57 +0900

    이코테: 큰 수의 법칙 (2)

--- coding-test-for-finding-jobs/큰-수의-법칙.js
+++ coding-test-for-finding-jobs/큰-수의-법칙.js
@@ -1,18 +1,7 @@
 const solution = (m, k, numbers) => {
   const [first, second] = numbers.sort((a, b) => b - a);
-  let answer = 0;
-  let count = 0;
-  while (m > 0) {
-    if (count < k) {
-      answer += first;
-      count++;
-    } else {
-      answer += second;
-      count = 0;
-    }
-    m--;
-  }
-  return answer;
+  const count = Math.floor(m / (k + 1)) * k + (m % (k + 1));
+  return first * count + second * (m - count);
 };
 
 const answer = solution(8, 3, [2, 4, 5, 4, 6]);
