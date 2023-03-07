7133cf429b2dc74b73efacf200a704c3047fa978
Hadam Cho <chohadam49@gmail.com>
2023-03-07 15:44:51 +0900

    이코테: 음료수 얼려 먹기 (2)

--- coding-test-for-finding-jobs/음료수-얼려-먹기.js
+++ coding-test-for-finding-jobs/음료수-얼려-먹기.js
@@ -1,41 +1,28 @@
-const dx = [0, 0, -1, 1];
-const dy = [-1, 1, 0, 0];
-
 const solution = (N, M, ice) => {
   ice = ice.split(" ").map((row) => row.split("").map((n) => Number(n)));
 
-  const bfs = (startX, startY) => {
-    const queue = [[startX, startY]];
-    ice[startY][startX] = 1;
-    while (queue.length !== 0) {
-      const [x, y] = queue.shift();
-      for (let i = 0; i < 4; i++) {
-        const [nx, ny] = [x + dx[i], y + dy[i]];
-        if (nx < 0 || ny < 0 || M <= nx || N <= ny || ice[ny][nx]) {
-          continue;
-        }
-        ice[ny][nx] = 1;
-        queue.push([nx, ny]);
-      }
+  const dfs = (x, y) => {
+    if (x < 0 || y < 0 || M <= x || N <= y) {
+      return false;
     }
+    if (ice[y][x] === 0) {
+      ice[y][x] = 1;
+      dfs(x, y - 1);
+      dfs(x, y + 1);
+      dfs(x - 1, y);
+      dfs(x + 1, y);
+      return true;
+    }
+    return false;
   };
 
   let count = 0;
-  while (true) {
-    let i = 0;
-    let j = 0;
-    for (; i < N; i++) {
-      j = ice[i].indexOf(0);
-      if (j !== -1) {
-        break;
+  for (let i = 0; i < N; i++) {
+    for (let j = 0; j < M; j++) {
+      if (dfs(j, i)) {
+        count++;
       }
     }
-    console.log(i, j);
-    if (j === -1) {
-      break;
-    }
-    bfs(j, i);
-    count += 1;
   }
 
   return count;
