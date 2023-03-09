72d42e34a2e868ad0092b10937687e1bc2e6309b
Hadam Cho <chohadam49@gmail.com>
2023-03-10 01:22:39 +0900

    이코테: 연구소 (2)

--- coding-test-for-finding-jobs/연구소.js
+++ coding-test-for-finding-jobs/연구소.js
@@ -3,57 +3,57 @@ const dy = [-1, 1, 0, 0];
 
 const solution = (N, M, map) => {
   map = map.map((row) => row.split(" ").map((n) => Number(n)));
+  const temp = new Array(N).fill().map((_) => new Array(M));
 
-  const fillVirus = (map) => {
+  const copy = () => {
     for (let i = 0; i < N; i++) {
-      for (let j = 0; j < M; j++) {
-        if (map[i][j] === 2) {
-          const queue = [[i, j]];
-          while (queue.length) {
-            const [y, x] = queue.shift();
-            for (let k = 0; k < 4; k++) {
-              const [ny, nx] = [y + dy[k], x + dx[k]];
-              if (ny < 0 || nx < 0 || N <= ny || M <= nx) {
-                continue;
-              }
-              if (map[ny][nx] === 0) {
-                map[ny][nx] = 2;
-                queue.push([ny, nx]);
-              }
-            }
-          }
-        }
+      for (let j = 0; j < map[i].length; j++) {
+        temp[i][j] = map[i][j];
+      }
+    }
+  };
+
+  const virus = (y, x) => {
+    for (let i = 0; i < 4; i++) {
+      const [ny, nx] = [y + dy[i], x + dx[i]];
+      if (ny < 0 || nx < 0 || N <= ny || M <= nx) {
+        continue;
+      }
+      if (temp[ny][nx] === 0) {
+        temp[ny][nx] = 2;
+        virus(ny, nx);
       }
     }
   };
 
   let answer = 0;
-  const dfs = (indexs) => {
-    if (indexs.length === 3) {
-      const newMap = JSON.parse(JSON.stringify(map));
-      for (const index of indexs) {
-        const i = Math.floor(index / M);
-        const j = index % M;
-        newMap[i][j] = 1;
+  const dfs = (count) => {
+    if (count === 3) {
+      copy();
+      for (let i = 0; i < N; i++) {
+        for (let j = 0; j < M; j++) {
+          if (temp[i][j] === 2) {
+            virus(i, j);
+          }
+        }
       }
-      fillVirus(newMap);
-      const count = newMap.flat().filter((n) => n === 0).length;
+      const count = temp.flat().filter((n) => n === 0).length;
       answer = Math.max(answer, count);
       return;
     }
 
-    const lastIndex = indexs.length ? indexs[indexs.length - 1] : -1;
-    for (let index = lastIndex + 1; index < N * M; index++) {
-      const i = Math.floor(index / M);
-      const j = index % M;
-      if (map[i][j] !== 0) {
-        continue;
+    for (let i = 0; i < N; i++) {
+      for (let j = 0; j < M; j++) {
+        if (map[i][j] === 0) {
+          map[i][j] = 1;
+          dfs(count + 1);
+          map[i][j] = 0;
+        }
       }
-      dfs([...indexs, index]);
     }
   };
 
-  dfs([]);
+  dfs(0);
   return answer;
 };
 
