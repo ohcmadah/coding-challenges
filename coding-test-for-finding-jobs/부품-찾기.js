48a6a40a540573fba1c4122bba05abb29ab41fff
Hadam Cho <chohadam49@gmail.com>
2023-03-23 16:02:36 +0900

    이코테: 부품 찾기 (이진 탐색)

--- coding-test-for-finding-jobs/부품-찾기.js
+++ coding-test-for-finding-jobs/부품-찾기.js
@@ -1,13 +1,32 @@
 const solution = (N, components, M, order) => {
-  const set = new Set(components.split(" "));
+  components = components.split(" ").sort((a, b) => Number(a) - Number(b));
+
+  const search = (arr, target, start, end) => {
+    if (start > end) {
+      return -1;
+    }
+
+    const mid = Math.floor((start + end) / 2);
+    const cur = Number(arr[mid]);
+    target = Number(target);
+    if (cur === target) {
+      return mid;
+    }
+
+    if (cur < target) {
+      return search(arr, target, mid + 1, end);
+    } else {
+      return search(arr, target, start, mid - 1);
+    }
+  };
 
   const result = order.split(" ").map((o) => {
-    if (set.has(o)) {
+    const index = search(components, o, 0, components.length) !== -1;
+    if (index) {
       return "yes";
     }
     return "no";
   });
-
   return result;
 };
 
